d0e851d3fa6dc9794af6dd621817815b7cfc9c32
Zishuo Zheng <Wind-Wing>
2021-02-25 21:23:48 +0800

    Delete/Rename some files

---
+++ regional_model.py
@@ -0,0 +1,14 @@
+import numpy as np
+from structure_mask import StructureMask
+
+
+class RegionalProjectionMatrix(object):
+    def __init__(self, voxel_projection_matrix, region_id_list):
+        self.voxel_matrix = voxel_projection_matrix
+        self.id_list = region_id_list
+
+    def get_regional_projection_matrix(self):
+        num_regions = len(self.id_list)
+        mat = np.zeros([num_regions, num_regions])
+
+
