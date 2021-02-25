6b8ef1991bb1e17a36a3e88211068971b09db5e5
Zishuo Zheng <Wind-Wing>
2021-02-25 20:39:46 +0800

    Add voxel_model.py - Inital implement of voxel model using Nadaraya Watson method to calculate projection density matrix

--- voxel_model.py
+++ voxel_model.py
@@ -1,144 +1,63 @@
-"""
-Module containing object :class:`VoxelModel`. The only difference between this
-estimator and :class:`NadarayaWatson` is that class:`VoxelModel` one treats the
-full injection volume (masked and flattened) as the input data `X` instead of
-only the centroids. Thus, :class:'VoxelModel` must be instatiated with the
-array of source_voxels, and the input data `X` must be passed as a tuple of arrays
-: (centroids, injections) or a concatenated array.
-"""
-# Authors: Joseph Knox <josephk@alleninstitute.org>
-# License: Allen Institute Software License
-
-from __future__ import division
-
-from scipy.sparse import issparse
-from sklearn.utils.validation import check_is_fitted
-from NadarayaWatson import NadarayaWatson
+import numpy as np
+from sklearn.metrics.pairwise import pairwise_kernels
+from structure_mask import StructureMask
 
 
-class VoxelModel(NadarayaWatson):
-    """Voxel-scale interpolation model for mesoscale connectivity.
+class VoxelModel(object):
+    def __init__(self, gamma):
+        self.gamma = gamma
+        self.kernel_type = "rbf"
 
-    Parameters
-    ----------
-    source_voxels : array-like, shape=(n_voxels, 3)
-        List of voxel coordinates at which to interpolate.
+    # x - [num_voxel, 3]
+    # y - [num_experience, 3]
+    # Return: kernel - [num_voxel, num_experience]
+    def _get_kernel(self, x, y):
+        params = {"gamma": self.gamma}
+        kernel = pairwise_kernels(x, y, metric=self.kernel_type, filter_params=True, **params)
+        return kernel
 
-    Examples
-    --------
-    >>> from mcmodels.core import VoxelModelCache
-    >>> from mcmodels.models import VoxelModel
-    >>> cache = VoxelModelCache
-    >>> # get cortical experiment data
-    >>> cortex_data = cache.get_experiment_data(injection_structure_ids=[315])
-    >>> source_voxels = cortex_data.source_mask.coordinates
-    >>> reg = VoxelModel(source_voxels)
-    >>> reg.fit((cortex_data.centroids, cortex_data.injections))
-    VoxelModel(source_voxels=array([[ ... ]]))
+    # kernel - [num_voxel, num_experience]
+    # normalize on num_experience axis
+    @staticmethod
+    def _normalize_kernel(kernel):
+        _sum = np.sum(kernel, axis=1, keepdims=True)
+        _kernel = kernel / _sum
+        return _sum
 
-    References
-    ----------
-    Knox et al. 'High resolution data-driven model of the mouse connectome'.
-        bioRxiv 293019; doi: https://doi.org/10.1101/293019
+    # Calculate projection strength using Nadaraya Watson Method
+    # x - [num_voxel, 3] Coordinate of source voxels that need to predict its projection density
+    # y - [num_experience, 3] Centroid injection coordinate of selected experiences
+    # projection_density - [num_experiences, num_voxel] Normalized projection density from selected experiences
+    # Return: projection weight matrix - [num_voxel, num_voxel] a[i][j] means i -> j 's projection strength
+    def _calc_projection_matrix(self, x, y, projection_density):
+        kernel = self._get_kernel(x, y)
+        normalized_kernel = self._normalize_kernel(kernel)
+        projection_matrix = np.matmul(normalized_kernel, projection_density)
+        return projection_matrix
 
-    See also
-    --------
-    NadarayaWatson
-    """
+    def get_projection_matrix(self, exp_list, structure_id_list):
+        structure_mask = StructureMask()
+        mask = structure_mask.get_mask(structure_id_list)
+        voxel_coordinate = mask.nonzeros().transpose()
 
-    def __init__(self, source_voxels, gamma=None):
-        super(VoxelModel, self).__init__(gamma=gamma)
+        injection_centroid = [x.injection_centroid for x in exp_list]
+        normalized_projection = [x.normalized_projection_density for x in exp_list]
 
-        self.source_voxels = source_voxels
-        self.dimension = self.source_voxels.shape[1]
+        projection_matrix = self._calc_projection_matrix(voxel_coordinate, injection_centroid, normalized_projection)
+        return projection_matrix
 
-    def _compute_weights(self, X, y=None):
-        K = self._get_kernel(X, y)
-        return self._normalize_kernel(K, overwrite=True)
 
-    def fit(self, X, y):
-        """Fit Voxel Model.
+if __name__ == "__main__":
+    voxel_model = VoxelModel()
 
-        NOTE : X is a concatenation (column wise) of the injection centroid
-            coordinates and the injection volumes. This choice was made to
-            be consistent with the sklearn.core.BaseEstimator fit and predict
-            schema
 
-        Parameters
-        ----------
-        X : {array-like, sparse matrix}, shape=(n_exps, 3+n_source_voxels)
-            Centroid coordinates concatenated with the injection density for
-            each training experiment.
 
-        y : {array-like, sparse matrix}, shape=(n_exps, n_target_voxels)
-            Normalized projection density for each training experiment
 
-        Returns
-        -------
-        self : returns an instance of self.
-        """
-        if isinstance(X, (list, tuple)):
-            centroids = X[0]
-        else:
-            # assume array
-            centroids = X[:, :self.dimension]
 
-        X, y = self._valid_input_data(centroids, y)
 
-        self.y_ = y
-        self.weights_ = self._compute_weights(self.source_voxels, centroids)
 
-        return self
 
-    def predict(self, X):
-        """Predict projection volumes given injection volumes.
 
-        NOTE : X is a concatenation (column wise) of the injection centroid
-            coordinates and the injection volumes. This choice was made to
-            be consistent with the sklearn.core.BaseEstimator fit and predict
-            schema
 
-        Parameters
-        ----------
-        X : {array-like, sparse matrix}, shape=(n_exps, 3+n_source_voxels)
-            Centroid coordinates concatenated with the injection density for
-            each test experiment.
 
-        Returns
-        -------
-        C : array, shape=(X.shape[0], y.shape[1])
-            Predicted normalized projection densities.
 
-        """
-        check_is_fitted(self, ["weights_", "y_"])
-
-        if isinstance(X, (list, tuple)):
-            injection = X[1]
-        else:
-            #assume array
-            injection = X[:, self.dimension:]
-
-        if len(injection.shape) == 1:
-            injection = injection.reshape(-1, 1)
-
-        # has to be of form sparse.dot(dense)
-        y = self.y_.toarray() if issparse(self.y_) else self.y_
-
-        return injection.dot(self.weights_).dot(y)
-
-    def get_weights(self):
-        """Overwrite of NadarayaWatson.get_weights."""
-        check_is_fitted(self, ["weights_", "y_"])
-        return self.weights_
-
-    @property
-    def weights(self):
-        """Return model weights."""
-        check_is_fitted(self, ["weights_", "y_"])
-        return self.weights_
-
-    @property
-    def nodes(self):
-        """Return model nodes (data)."""
-        check_is_fitted(self, ["weights_", "y_"])
-        return self.y_
