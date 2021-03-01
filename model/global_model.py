14e0138f1ffaca5eaf9ad8525dd0d2524d34f8ae
Zishuo Zheng <Wind-Wing>
2021-03-01 21:16:53 +0800

    Update global_model.py - public get_region_projection()

--- model/global_model.py
+++ model/global_model.py
@@ -1,31 +1,27 @@
 import numpy as np
 from utils.structure_mask import StructureMask
-from voxel_model import VoxelModel
+from model.voxel_model import VoxelModel
 
 
 # TODO: compare with regional model and homogeneous model
 # Using Nadaraya Watson method to calculate region projection matrix based on global data
 class GlobalModel(object):
-    def __init__(self, region_id_list, exp_list):
-        self.id_list = region_id_list
-        self.exp_list = exp_list
-        self.gamma = 0.013
+    def __init__(self, gamma=0.013):
+        self.gamma = gamma
 
     # Return - mat[i][j]: i -> j projection
-    def get_regional_projection_matrix(self):
-        num_regions = len(self.id_list)
-        mat = np.zeros([num_regions, num_regions])
+    def get_regional_projection_matrix(self, source_structure_id_list, target_structure_id_list, exp_list):
+        mat = np.zeros([len(source_structure_id_list), len(target_structure_id_list)])
 
-        projection_list = np.array([self._get_region_projection(x) for x in self.id_list])
+        _get_region_projection_vector = np.vectorize(self.get_region_projection)
+        projection_list = _get_region_projection_vector(source_structure_id_list, target_structure_id_list, exp_list)
 
         structure_mask = StructureMask()
-        for i in range(num_regions):
-            mask_idx = structure_mask.get_mask([self.id_list[i]]).nonzero()
+        for i in range(len(source_structure_id_list)):
+            mask_idx = structure_mask.get_mask([source_structure_id_list[i]]).nonzero()
             region_projection_list = np.array([x[mask_idx] for x in projection_list])
             assert len(region_projection_list.shape) == 2
             mat[i, :] = np.mean(region_projection_list, axis=1)
-
-        # mat = self._normalize_projection_matrix(mat)
         return mat
 
     @staticmethod
@@ -33,21 +29,10 @@ class GlobalModel(object):
         _sum = np.sum(mat, axis=1)
         return mat / _sum
 
-    def _get_region_projection(self, source_structure_id):
+    # Return - volume[x][y][z]: voxel level projection density from source regions
+    def get_region_projection(self, source_structure_id, target_structure_id_list, exp_list):
         print("Calculating projection of %d" % source_structure_id)
         voxel_model = VoxelModel(self.gamma)
         voxel_projection_matrix = voxel_model.get_voxel_mean_projection_matrix(
-            [source_structure_id],
-            self.id_list,
-            self.exp_list)
+            [source_structure_id], target_structure_id_list, exp_list)
         return voxel_projection_matrix
-
-
-
-
-
-
-
-
-
-
