790488ca2f4fcbaaa0d58317257772c6098d30f1
Zishuo Zheng <Wind-Wing>
2021-03-02 19:56:48 +0800

    Update global_model.py - lpsilateral and contralateral projection

--- model/global_model.py
+++ model/global_model.py
@@ -11,14 +11,34 @@ class GlobalModel(object):
 
     # Return - mat[i][j]: i -> j projection
     def get_regional_projection_matrix(self, source_structure_id_list, target_structure_id_list, exp_list):
-        mat = np.zeros([len(source_structure_id_list), len(target_structure_id_list)])
+        assert all(exp_list.injection_centroid[2] >= 57)
 
         _get_region_projection_vector = np.vectorize(self.get_region_projection)
         projection_list = _get_region_projection_vector(source_structure_id_list, target_structure_id_list, exp_list)
 
+        contralateral_mat = self._get_hemisphere_regional_projection_matrix(
+            projection_list,
+            source_structure_id_list,
+            target_structure_id_list,
+            0)
+
+        lpsilateral_mat = self._get_hemisphere_regional_projection_matrix(
+            projection_list,
+            source_structure_id_list,
+            target_structure_id_list,
+            1)
+        return lpsilateral_mat, contralateral_mat
+
+    @staticmethod
+    def _get_hemisphere_regional_projection_matrix(
+            projection_list,
+            source_structure_id_list,
+            target_structure_id_list,
+            target_hemisphere):
         structure_mask = StructureMask()
+        mat = np.zeros([len(source_structure_id_list), len(target_structure_id_list)])
         for i in range(len(source_structure_id_list)):
-            mask_idx = structure_mask.get_mask([source_structure_id_list[i]]).nonzero()
+            mask_idx = structure_mask.get_mask([source_structure_id_list[i]], target_hemisphere).nonzero()
             region_projection_list = np.array([x[mask_idx] for x in projection_list])
             assert len(region_projection_list.shape) == 2
             mat[i, :] = np.mean(region_projection_list, axis=1)
@@ -31,6 +51,7 @@ class GlobalModel(object):
 
     # Return - volume[x][y][z]: voxel level projection density from source regions
     def get_region_projection(self, source_structure_id, target_structure_id_list, exp_list):
+        assert all(exp_list.injection_centroid[2] >= 57)
         print("Calculating projection of %d" % source_structure_id)
         voxel_model = VoxelModel(self.gamma)
         voxel_projection_matrix = voxel_model.get_voxel_mean_projection_matrix(
