579c06e4fc77a78bf8fb10a88f0d6627462a991e
Zishuo Zheng <Wind-Wing>
2021-03-05 16:47:47 +0800

    Refactor experience.py

--- utils/experiment.py
+++ utils/experiment.py
@@ -1,54 +1,63 @@
 import os
-from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
 import numpy as np
-import matplotlib.pyplot as plt
+from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
 from retry import retry
+from utils.constants import *
 
 
 class Experiment(object):
-    def __init__(self, exp_dict, resolution):
-        self.id = exp_dict["id"]
-        self.structure_id = exp_dict["structure_id"]
-        self.structure_name = exp_dict["structure_name"]
-        self.injection_structures = exp_dict["injection_structures"]
-        self.primary_injection_structure = exp_dict["primary_injection_structure"]
+    def __init__(self, exp_dict, structure_mask_idx=None):
+        # self.id = exp_dict["id"]
+        # self.structure_id = exp_dict["structure_id"]
+        # self.structure_name = exp_dict["structure_name"]
+        # self.injection_structures = exp_dict["injection_structures"]
+        # self.primary_injection_structure = exp_dict["primary_injection_structure"]
         # self.injection_volume = exp_dict["injection_volume"]
         # self.injection_coordinate = (exp_dict["injection_x"], exp_dict["injection_y"], exp_dict["injection_z"])
 
-        # Density - fraction of fluorescing pixels per voxel
-        self.cache = MouseConnectivityCache(resolution=resolution)
-        self._fetch_data_from_server()
-        self.hemisphere = self.injection_centroid[2] >= 57 # 0 left, 1 right
+        injection_density, projection_density = self._fetch_data_from_server(exp_dict['id'])
+        self.injection_centroid = self._calc_injection_centroid(injection_density)
+        self.hemisphere = self._calc_hemisphere()
+        self.normalized_projection_density = self._calc_normalized_projection_density(
+            injection_density, projection_density, structure_mask_idx)
 
+    @staticmethod
     @retry()
-    def _fetch_data_from_server(self):
+    def _fetch_data_from_server(exp_id):
         # Projection density is superset of injection density.
-        self.valid_mask = self.cache.get_data_mask(self.id)[0]
-        # self.injection_fraction = self.cache.get_injection_fraction(self.id)[0]
-        self.projection_density = self.cache.get_projection_density(self.id)[0] * self.valid_mask
-        self.injection_density = self.cache.get_injection_density(self.id)[0] * self.valid_mask
+        # Density - fraction of fluorescing pixels per voxel
+        cache = MouseConnectivityCache(resolution=RESOLUTION)
+        valid_mask = cache.get_data_mask(exp_id)[0]
+        injection_density = cache.get_injection_density(exp_id)[0] * valid_mask
+        projection_density = cache.get_projection_density(exp_id)[0] * valid_mask
+        return injection_density, projection_density
 
-    @property
-    def injection_centroid(self):
+    @staticmethod
+    def _calc_injection_centroid(injection_density):
         # centroid = coords * weight / sum(weight)
-        nnz = self.injection_density.nonzero()
+        nnz = injection_density.nonzero()
         coords = np.vstack(nnz)
-        return np.dot(coords, self.injection_density[nnz]) / self.injection_density.sum()
+        return np.dot(coords, injection_density[nnz]) / injection_density.sum()
 
-    @property
-    def normalized_projection_density(self):
-        return self.projection_density / np.sum(self.injection_density)
+    def _calc_hemisphere(self):
+        if self.injection_centroid[VERTICAL_PLANE_IDX] >= VOXEL_SHAPE[VERTICAL_PLANE_IDX] // 2:
+            hemisphere = R_HEMISPHERE
+        else:
+            hemisphere = L_HEMISPHERE
+        return hemisphere
 
-    def visualize(self):
-        plt.pcolor(self.projection_density[100, :, :])
-        plt.show()
-        plt.clf()
+    @staticmethod
+    def _calc_normalized_projection_density(injection_density, projection_density, structure_mask_idx):
+        normalized_projection_density = projection_density / np.sum(injection_density)
+        if structure_mask_idx is not None:
+            normalized_projection_density = normalized_projection_density[structure_mask_idx]
+        return normalized_projection_density
 
     # All experience will be flip to R domain hemisphere
     def flip(self):
-        self.valid_mask = np.flip(self.valid_mask, axis=2)
-        self.projection_density = np.flip(self.projection_density, axis=2)
-        self.injection_density = np.flip(self.injection_density, axis=2)
+        # self.projection_density = np.flip(self.projection_density, axis=2)
+        # self.injection_density = np.flip(self.injection_density, axis=2)
+        self.normalized_projection_density = np.flip(self.normalized_projection_density, axis=2)
 
 
 def main():
@@ -59,12 +68,9 @@ def main():
     cortex_region_ids = [x['id'] for x in cortex_structures]
     all_experiments = mcc.get_experiments(dataframe=False, injection_structure_ids=cortex_region_ids)
 
-    exp_formater = lambda exp: Experiment(exp, 100)
-    experience_list = list(map(exp_formater, all_experiments))
-    centroid = [x.injection_centroid for x in experience_list]
-    x = np.array([x[2] for x in centroid])
-    print(np.sum(x >= 57))
-    print(np.sum(x < 57))
+    exp_func = lambda exp: Experiment(exp)
+    experience_list = list(map(exp_func, all_experiments))
+    print(np.mean([x.hemisphere for x in experience_list]))
 
 
 if __name__ == "__main__":
