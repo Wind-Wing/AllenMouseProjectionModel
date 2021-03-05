199e96b231b94d0eacfc47a34e5f1e6cb0849033
Zishuo Zheng <Wind-Wing>
2021-03-05 19:32:27 +0800

    Refactor structure_mask.py

--- utils/structure_mask.py
+++ utils/structure_mask.py
@@ -2,46 +2,42 @@ from queue import Queue
 import numpy as np
 from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
 from allensdk.api.queries.ontologies_api import OntologiesApi
+from utils.constants import *
 
 
 class StructureMask(object):
     def __init__(self):
-        self.mcc = MouseConnectivityCache(resolution=100)
+        self.mcc = MouseConnectivityCache(resolution=RESOLUTION)
         self.structure_tree = self.mcc.get_structure_tree()
         self.annotations = self.mcc.get_annotation_volume()[0]
 
-    # Hemisphere - 0 left, 1 right, 2 both
-    def get_mask(self, structure_id_list, hemisphere=2):
-        mask_list = []
-        for id in structure_id_list:
-            fine_grained_id_list = self._get_fine_grained_annotation_list(id)
-            mask = None
-            for i in fine_grained_id_list:
-                _mask = (self.annotations == i)
-                if mask is None:
-                    mask = _mask
-                else:
-                    mask = np.logical_or(mask, _mask)
+    def get_mask_idx(self, structure_id_list, hemisphere=BOTH_HEMISPHERE):
+        mask = self.get_mask(structure_id_list, hemisphere)
+        return mask.nonzero()
 
-            if np.sum(mask) == 0:
-                raise Exception("Structure id error: %d is not a valid id." % id)
-            else:
-                mask_list.append(mask)
-
-        mask = mask_list[0]
-        for m in mask_list[1:]:
-            mask = np.logical_or(mask, m)
-
+    def get_mask(self, structure_id_list, hemisphere=BOTH_HEMISPHERE):
+        masks = [self._get_mask(x) for x in structure_id_list]
+        mask = np.logical_or.reduce(masks)
         mask = self._get_hemisphere(mask, hemisphere)
+        return mask
+
+    def _get_mask(self, structure_id):
+        fine_grained_id_list = self._get_fine_grained_annotation_list(structure_id)
+
+        masks = [self.annotations == x for x in fine_grained_id_list]
+        mask = np.logical_or.reduce(masks)
+
+        if np.sum(mask) == 0:
+            raise Exception("Structure id error: %d is not a valid id." % structure_id)
         return mask
 
     @staticmethod
     def _get_hemisphere(mask, hemisphere):
-        assert hemisphere in (0, 1, 2)
-        if hemisphere == 0:
-            mask[:, :, 57:] = 0
-        elif hemisphere == 1:
-            mask[:, :, :57] = 0
+        middle_line = VOXEL_SHAPE[VERTICAL_PLANE_IDX] // 2
+        if hemisphere == L_HEMISPHERE:
+            mask[:, :, middle_line:] = 0
+        elif hemisphere == R_HEMISPHERE:
+            mask[:, :, :middle_line] = 0
         return mask
 
     def _get_fine_grained_annotation_list(self, structure_id):
@@ -56,7 +52,6 @@ class StructureMask(object):
             _child_ids = self.structure_tree.child_ids([_id])[0]
             for i in _child_ids:
                 query_queue.put(i)
-
         return list(annotation_id_list)
 
 
