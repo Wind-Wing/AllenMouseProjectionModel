0521743d445300bb8800b71a8750e59e130ee435
Zishuo Zheng <Wind-Wing>
2021-03-10 18:54:33 +0800

    Update nadaraya_watson_model.py - refactor

--- model/nadaraya_watson_model.py
+++ model/nadaraya_watson_model.py
@@ -6,6 +6,8 @@ class NadarayaWatsonModel(object):
     def __init__(self, gamma):
         self.gamma = gamma
         self.kernel_type = "rbf"
+        self.training_x = None
+        self.training_y = None
 
     # x - [num_voxel, 3]
     # y - [num_experience, 3]
@@ -23,18 +25,32 @@ class NadarayaWatsonModel(object):
         _kernel = kernel / _sum
         return _kernel
 
+    # x - [num_samples, num_features]
+    # y - [num_samples, num_predicts]
+    def training(self, x, y):
+        self.training_x = x
+        self.training_y = y
+
+    # x - [num_samples, num_features]
+    # Return: y - [num_samples, num_predicts]
+    def predict(self, x):
+        kernel = self._get_kernel(x, self.training_x)
+        normalized_kernel = self._normalize_kernel(kernel)
+        projection_matrix = np.matmul(normalized_kernel, self.training_y)
+        return projection_matrix
+
     # Calculate projection strength using Nadaraya Watson Method
     # x - [num_regional_voxel, 3] Coordinate of source voxels that need to predict its projection density
     # y - [num_experience, 3] Centroid injection coordinate of selected experiences
     # projection_density - [num_experiences, num_target_voxel] Normalized projection density from selected experiences
     # Return: projection weight matrix - [num_regional_voxel, num_target_voxel] a[i][j]: i -> j 's projection strength
-    def _calc_projection_matrix(self, x, y, projection_density):
-        kernel = self._get_kernel(x, y)
-        normalized_kernel = self._normalize_kernel(kernel)
-        projection_matrix = np.matmul(normalized_kernel, projection_density)
-        return projection_matrix
+    # def _calc_projection_matrix(self, x, y, projection_density):
+    #     kernel = self._get_kernel(x, y)
+    #     normalized_kernel = self._normalize_kernel(kernel)
+    #     projection_matrix = np.matmul(normalized_kernel, projection_density)
+    #     return projection_matrix
 
-    # source_id_list - [num_structure_id] Voxels in which structures that needed to predict it's projection
+    # source_mask_idx - [num_source_voxel, 3] Coordinate of voxels that needed to predict it's projection
     #                   taking this voxel as injection point.
     # exp_list - [num_exp] experiences as training data that used to predict the unknown projection from above voxels.
     # Return: projection_matrix - [num_target_voxel] aggregated density of projection from all voxels in source region
@@ -47,12 +63,14 @@ class NadarayaWatsonModel(object):
         injection_centroid = np.array([x.injection_centroid for x in exp_list])
         normalized_projection = np.array([x.normalized_projection_density for x in exp_list])
 
-        projection_matrix_list = self._calc_projection_matrix(
-            source_voxel_coordinate,
-            injection_centroid,
-            normalized_projection)
+        self.training(injection_centroid, normalized_projection)
+        projection_matrix_list = self.predict(source_voxel_coordinate)
+
         projection_matrix = aggregate_func(projection_matrix_list, axis=0)
         return projection_matrix
+
+    def set_gamma(self, gamma):
+        self.gamma = gamma
 
 
 if __name__ == "__main__":
